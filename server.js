426cc5fbeb88f4d32afa076dcc980a6068c87cf0
Lee Tsai <lee.x.tsai@gmail.com>
2016-07-07 19:00:52 -0700

    Fixed port

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+var app = express();
+var port = process.env.PORT || 1337;
+
 app.use(function(req,res,next) {
   console.log('/' + req.method + ' from ' + req.url);
   next();
@@ -49,6 +52,6 @@ app.listen(3000, function() {
 
 // do other stuff
 app.use(express.static(__dirname + '/public'))
-app.listen(1337, function() {
+app.listen(port, function() {
   console.log('App listening on port 1337!');
 });
