393b97ab8e1b7d879017c9aa97ec888fdf1ae2b9
Lee Tsai <lee.x.tsai@gmail.com>
2016-07-11 16:38:39 -0700

    [Feature] Completed yelp search module. Searches made dynamic and returns an array of filtered events

--- server/externalAPIs/yelp.js
+++ server/externalAPIs/yelp.js
@@ -8,21 +8,49 @@ var yelp = new Yelp({
   token_secret: process.env['TOKEN_SECRET']
 });
 
-var yelpSearch = yelp.search({term:'dinner', location: 'San Francisco'})
-  .then(function(data) {
-    console.log(data);
-  })
-  .catch(function(err) {
-    console.error(err);
-  });
+// var yelpSearch = function(search, location, cb) {
+//   yelp.search({term: search, location: location})
+//     .then(function(data) {
+//       cb(data);
+//       console.log(data);
+//     })
+//     .catch(function(err) {
+//       console.error(err);
+//     });
+//   };
 
-// yelp.business('yelp-san-francisco')
-//   .then(console.log)
-//   .catch(console.error);
-//
-// yelp.phoneSearch({phone: '+12223334444'})
-//   .then(console.log)
-//   .catch(console.error);
+var yelpSearch = function(search, location, limit, cb) {
+  yelp.search({term: search, location: location})
+    .then(function(data) {
+      // Show buisnesses only; not interested in coordinates
+      var businesses = data.businesses;
+      // Filter out currently closed businesses and those with ratings < 4
+      var filteredList = businesses.filter(function(el) {
+        return el.rating >= 4 &&
+               el.is_closed === false;
+      });
+      var truncatedList = filteredList.slice(0, limit);
+      // Create newArr to store categories we're interested in pulling
+      var newArr = [];
+      var finalList = truncatedList.map(function(el) {
+        // newArr.push({'business': el.name, 'rating': el.rating, 'rating_image': el.rating_img_url_small, 'phone': el.display_phone})
+        newArr.push({'business': el.name,
+                     'reviews': el.snippet_text,
+                     'phone': el.display_phone,
+                     'rating': el.rating,
+                     'rating_image': el.rating_img_url_small})
+      });
+      cb(newArr);
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+  };
+
+  // // *** UNCOMMENT TO SEE LOG OF QUERY ***
+  // yelpSearch("lunch", "san francisco", 10, function(element) {
+  //   console.log(element);
+  // });
 
 module.exports = {
   yelpSearch: yelpSearch,
