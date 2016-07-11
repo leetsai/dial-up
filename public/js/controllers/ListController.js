fc9861cbec47968597be348bee089a56cdc3de20
Lee Tsai <lee.x.tsai@gmail.com>
2016-07-11 11:27:16 -0700

    Cleaned up ListController

--- public/js/controllers/ListController.js
+++ public/js/controllers/ListController.js
@@ -2,7 +2,6 @@ angular.module('App.listCtrl', [])
 
 // Not working at the moment - switching over to list directive to stay on same page
 .controller('ListController', ['$scope', 'Ideas', function($scope, Ideas) {
-<<<<<<< 75f58f5036338ea180d6c2ca84c01192e1f349f3
   $scope.data = {};
   $scope.idea = Ideas.currentIdea.data;
   $scope.data.events = [{
@@ -11,9 +10,4 @@ angular.module('App.listCtrl', [])
     description: 'Biking, on a mountain'
   }]
   console.log($scope.data)
-
-=======
-  $scope.data = Ideas.currentIdea();
-  console.log($scope.data);
->>>>>>> [Feature] Comverting yelpSearch into a function
 }])
