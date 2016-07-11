31afcb1a138cb5e0435b99e9ad8b98b3ecb9c9b0
Lee Tsai <lee.x.tsai@gmail.com>
2016-07-11 11:16:53 -0700

    [Feature] Comverting yelpSearch into a function

--- public/js/controllers/ListController.js
+++ public/js/controllers/ListController.js
@@ -2,6 +2,7 @@ angular.module('App.listCtrl', [])
 
 // Not working at the moment - switching over to list directive to stay on same page
 .controller('ListController', ['$scope', 'Ideas', function($scope, Ideas) {
+<<<<<<< 75f58f5036338ea180d6c2ca84c01192e1f349f3
   $scope.data = {};
   $scope.idea = Ideas.currentIdea.data;
   $scope.data.events = [{
@@ -11,4 +12,8 @@ angular.module('App.listCtrl', [])
   }]
   console.log($scope.data)
 
+=======
+  $scope.data = Ideas.currentIdea();
+  console.log($scope.data);
+>>>>>>> [Feature] Comverting yelpSearch into a function
 }])
