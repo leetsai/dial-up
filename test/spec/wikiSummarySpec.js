c988ba289f1adc4b29e2c04283196022a712f9ec
Damon <damonnguyen92@gmail.com>
2016-07-11 16:22:46 -0700

    Add file for testing

---
+++ test/spec/wikiSummarySpec.js
@@ -0,0 +1,10 @@
+//test suite for wikipedia:
+var wikipedia = require('../../server/externalAPIs/wikipedia.js');
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('wikipedia', function() {
+  it('should take a string as an input', function() {
+    
+  });
+})
