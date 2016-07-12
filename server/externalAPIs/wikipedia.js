753a199ec476ca4f86cf858a4539b66d8f6a2d1c
Damon <damonnguyen92@gmail.com>
2016-07-11 18:23:00 -0700

    Implement URI encoder

--- server/externalAPIs/wikipedia.js
+++ server/externalAPIs/wikipedia.js
@@ -4,6 +4,7 @@ var wikiSummary = function(search, callback) {
   search = encodeURI(search); //handle escaped characters
   request('https://en.wikipedia.org/w/api.php?action=opensearch&search=' + search + '&limit=1&format=json', function (error, response, body) {
     if (!error && response.statusCode == 200) {
+      results = body;
       callback(body);
     } else {
       console.log(error);
