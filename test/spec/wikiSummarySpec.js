a7695453609736c5c792a534c01305762116e87a
Damon <damonnguyen92@gmail.com>
2016-07-12 14:51:06 -0700

    Add test for wikiSummarySpec.js

--- test/spec/wikiSummarySpec.js
+++ test/spec/wikiSummarySpec.js
@@ -7,4 +7,33 @@ describe('wikipedia', function() {
   it('should be a function', function() {
     expect(wikipedia.wikiSearch).to.be.a('function');
   });
+
+  it('should generate an object.', function() {
+    var wikiData;
+    wikipedia.wikiSearch('Basketball', function(data) {
+      wikiData = data;
+      expect(wikiData).to.be.a('object');
+    });
+  });
+
+  it('should have: searchTerm, description, and link properties.', function() {
+    wikipedia.wikiSearch('Basketball', function(data) {
+      expect(data).to.have.any.keys('searchTerm', 'description', 'link');
+    });
+  });
+
+  it('values should be string', function() {
+    wikipedia.wikiSearch('Basketball', function(data) {
+      for (var key in data) {
+        expect(data[key]).to.be.a('string');
+      }
+    });
+  });
+
+  // it('should encode argument & integrate into url.', function() {
+  //   wikipedia.wikiSearch('Basketball', function(data) {
+  //     expect(data).to.be.a('string');
+  //   });
+  // });
+
 })
