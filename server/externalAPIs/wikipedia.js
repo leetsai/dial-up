d4c8ea9b523d8a52f5981fc40a4a9817958e2e44
Ian Mrak <ianmrak@gmail.com>
2016-07-16 19:40:03 -0700

    [API] added a default msg when description doesn't load

--- server/externalAPIs/wikipedia.js
+++ server/externalAPIs/wikipedia.js
@@ -2,15 +2,15 @@
 var request = require('request');
 
 var wikiSummary = function(search, callback) {
-  search = encodeURI(search); //handle escaped characters
-  request('https://en.wikipedia.org/w/api.php?action=opensearch&search=' + search + '&limit=1&format=json', function (error, response, body) {
+  urlSearch = encodeURI(search); //handle escaped characters
+  request('https://en.wikipedia.org/w/api.php?action=opensearch&search=' + urlSearch + '&limit=1&format=json', function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var results = {};
-      body = JSON.parse(body); //response data was a stringified array of subarrays => parsed 
+      body = JSON.parse(body); //response data was a stringified array of subarrays => parsed
       results.searchTerm = body[1][0]; //subzero to access element from array.
       results.description = body[2][0];
       results.link = body[3][0];
-
+      if (!results.description) { results.description = 'Here are some options near you...' }
       callback(results);
     } else {
       console.log(error);
